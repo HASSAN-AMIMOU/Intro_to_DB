b58ca61cb1f4b9447a40103ce8f9c6597813d477
hassan amimou <hassanamimou@gmail.com>
2025-02-02 17:27:13 +0100

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -3,9 +3,6 @@
 -- Inserting multiple rows into the 'customer' table
 INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES
-    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+    (2, 'Blessing Malik', 'bmalik@sandtech.com', "124 Happiness  Ave."),
     (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
     (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
-
--- Checking for the insertion of customer with customer_id = 2
-SELECT * FROM customer WHERE customer_id = 2;
